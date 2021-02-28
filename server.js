49a5fcb426099c5e6150fc31a8d5846525895b7f
Whooosp <vss180000@utdallas.edu>
2021-02-27 23:25:22 -0600

    using cors (hopefully)

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const connectDB = require('./config/db')
 const app = express()
+const cors = require('cors')
 
 // stuff
 require('dotenv').config()
@@ -13,8 +14,9 @@ app.use(express.json({ extended: false }))
 app.use(express.urlencoded({ extended: false }))
 app.get('/', (req, res) => res.send('API running'))
 
+app.use('/api', cors())
+
 app.use('/api/events', require('./routes/api/events'))
 
 const PORT = process.env.PORT || 8000
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
-
