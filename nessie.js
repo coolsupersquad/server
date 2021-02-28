ed97aabc213198db9c54723a64e9ffc901f75f50
joe2475 <33675219+joe2475@users.noreply.github.com>
2021-02-27 19:54:19 -0600

    Nessie functions

--- nessie.js
+++ nessie.js
@@ -1,11 +1,31 @@
 var request = require('superagent');
 
-var res = {
-    
-}
+var newCust = 
+{
+    first_name : "",
+    last_name : "",
+    address : {
+        street_number : "",
+        street_name : "",
+        city: "",
+        state: "",
+        zip: ""
+    }
+};
 
-request.get('http://api.reimaginebanking.com/atms?key=7e107b3e9f9368304927f930f444f6a3').end(function(res){
+
+
+function createCustomer()
+{
+    request.get('http://api.reimaginebanking.com/customers?key=7e107b3e9f9368304927f930f444f6a3')
+    .query(JSON.stringify(newCust))
+    .end(function(res){
+    console.log(res.body); 
     foo(res.status);
     bar(res.body); //do something
 });
+
+
+
+};
 
